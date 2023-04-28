5a08cea09f52d839aa23da733552fd4d6c6c86bf
reevugg <132049450+reevugg@users.noreply.github.com>
2023-04-28 17:23:13 +0800

    Add files via upload

--- ActuationND/ActuationND.ino
+++ ActuationND/ActuationND.ino
@@ -1,7 +1,12 @@
+#include <IRremote.h>
+#define IR_LED_PIN 7
 int relay1 = 2;
 int relay2 = 3;
 int count;
 int a;
+IRsend irsend;// Initialize the IR sender
+
+// Enabling bluetooth serial connection to achieve the slave control.
 void setup() {
   Serial.begin( 9600 );
   pinMode(relay1, OUTPUT);
@@ -18,8 +23,14 @@ void loop() {
 //codes for actuation 
 
 //Light control ser 1 ser 2
-if (a==1){digitalWrite(relay1, LOW);}
-if (a==2){ digitalWrite(relay1, HIGH);}
-if (a==3){digitalWrite(relay2, LOW);}
-if (a==4){ digitalWrite(relay2, HIGH);}
+if (a==1){digitalWrite(relay1, LOW);}//Light on
+if (a==2){ digitalWrite(relay1, HIGH);}//Light off
+if (a==3){digitalWrite(relay2, LOW);}//Appliance on
+if (a==4){ digitalWrite(relay2, HIGH);}//appliance off
+if (a==5)
+{
+  irsend.sendNEC(0x00FF30CFe, 32);//TV ON
+}
+
+
 }
