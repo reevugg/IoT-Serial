8a73b0731aba825f3819456566884e67f1eeac44
reevugg <132049450+reevugg@users.noreply.github.com>
2023-04-28 17:35:02 +0800

    Add files via upload

--- IR_receive/IR_receive.ino
+++ IR_receive/IR_receive.ino
@@ -1,33 +1,17 @@
-int led = 13;
+#include <IRremote.h>
 
-// the setup routine runs once when you press reset:
-void setup() { 
-  Serial.begin( 9600 );  
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+int IR_RECEIVE_PIN = 11; // Pin number where IR receiver is connected
+IRrecv irrecv(IR_RECEIVE_PIN);
+decode_results results;
+
+void setup() {
+  Serial.begin(9600); // Start serial communication at 9600 baud rate
+  irrecv.enableIRIn(); // Start the receiver
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // listen for the data
-  if ( Serial.available() > 0 ) {
-    // read a numbers from serial port
-    int count = Serial.parseInt();
-    Serial.print("INPUT?? ");
-    
-     // print out the received number
-    if (count > 0) {
-        Serial.print("You have input: ");
-        Serial.println(String(count));
-        // blink the LED
-        for (int lup = 0; lup < count; lup++) { 
-        // turn the pin on:
-        digitalWrite(led, HIGH);   
-        delay(500);                  
-        // turn the pin off:
-        digitalWrite(led, LOW);
-        delay(500);    
+  if (irrecv.decode(&results)) { // If a code is received
+    Serial.println(results.value, HEX); // Print the received code in hexadecimal format
+    irrecv.resume(); // Receive the next code
   }
-    }
-  }               // wait for a second
 }
